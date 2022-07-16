208abe2fcede5241e4705bce4412199776196b19
Ruixuan Huang <ryan_0149@qq.com>
2022-07-16 23:29:27 +0800

    add more courses

--- src/constants/video.ts
+++ src/constants/video.ts
@@ -22,12 +22,66 @@ export const VIDEO_COURSES: BCourse[] = [
       'https://i1.hdslb.com/bfs/archive/151d7bd0b6e07e8d6fb4f679cfc40471e317832c.png',
     // noteFolderName: '新结构经济学2',
   },
-  // {
-  //   bvid: 'BV1c541157Te',
-  //   title: '货币银行学 黄益平',
-  //   cover:
-  //     'http://i2.hdslb.com/bfs/archive/632036014ed7535b426735817d078c0a79c0feb0.jpg',
-  // },
+  {
+    bvid: 'BV173411e7nm',
+    title: '微观经济学',
+    cover:
+      'http://i2.hdslb.com/bfs/archive/6852b5de43d8a6f23bc8ce246b8352dd7cb64313.jpg',
+  },
+  {
+    bvid: 'BV1GK4y1S7FF',
+    title: '宏观经济学',
+    cover:
+      'http://i0.hdslb.com/bfs/archive/22cfaa92525276a4a4cdc054c3e86fd61e1676c6.jpg',
+  },
+  {
+    bvid: 'BV1pW41117rs',
+    title: '高级计量经济',
+    cover:
+      'http://i0.hdslb.com/bfs/archive/224167951c0ac00f34708b3107a04a0ceed7d253.jpg',
+  },
+  {
+    bvid: 'BV1uU4y1a7zc',
+    title: '国际经济学',
+    cover:
+      'http://i2.hdslb.com/bfs/archive/0d512a2ca6b2ebfdfe2cace6966a2292b6b69b3f.jpg',
+  },
+  {
+    bvid: 'BV1wW411p73z',
+    title: '产业经济学',
+    cover:
+      'http://i2.hdslb.com/bfs/archive/3cff9ca4661b33fce22b4982a534263a17dfb213.png',
+  },
+  {
+    bvid: 'BV1k3411J7xB',
+    title: '财政学',
+    cover:
+      'http://i1.hdslb.com/bfs/archive/09b0b980d787310114e81ebfeebb257468aaffc5.jpg',
+  },
+  {
+    bvid: 'BV1bK411L7FR',
+    title: '公司金融',
+    cover:
+      'http://i2.hdslb.com/bfs/archive/14a308f46c6e0a348aee34f090b8d66f3971d35a.jpg',
+  },
+  {
+    bvid: 'BV1H5411P74w',
+    title: '证券投资学',
+    cover:
+      'http://i2.hdslb.com/bfs/archive/3cce8d70c4065396b195e0d4def5a8933c5758ed.jpg',
+  },
+  {
+    bvid: 'BV1c541157Te',
+    title: '货币银行学',
+    cover:
+      'http://i2.hdslb.com/bfs/archive/632036014ed7535b426735817d078c0a79c0feb0.jpg',
+  },
+  {
+    bvid: 'BV1LU4y1h7Nb',
+    title: '高级计量经济（英）',
+    cover:
+      'http://i1.hdslb.com/bfs/archive/ba6a13bfdbe241dd0f619f1a729f8bdfe2c98b82.png',
+  },
   // {
   //   bvid: 'BV1qW4y1r7KT',
   //   title: '《华尔街》纪录片 ',
